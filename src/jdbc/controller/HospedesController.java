a14c1c222ed803204550b7f8832192ec119583e4
Mateus Lisboa <mathewslysboa@gmail.com>
2023-09-17 21:32:17 -0300

    feat(jdbc/controller): Criação das classes de controle para comunicação entre as views e as DAOs

---
+++ src/jdbc/controller/HospedesController.java
@@ -0,0 +1,38 @@
+package jdbc.controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
+
+import jdb.factory.ConnectionFactory;
+import jdbc.dao.HospedesDao;
+import jdbc.modelo.Hospedes;
+
+public class HospedesController {
+    private HospedesDao hospedesDao;
+
+    public HospedesController() {
+        Connection connection = new ConnectionFactory().recuperarConexao();
+        this.hospedesDao = new HospedesDao(connection);
+    }
+
+    public void salvar(Hospedes hospedes) {
+        this.hospedesDao.salvar(hospedes);
+    }
+
+    public List<Hospedes> listarHospedes() {
+        return this.hospedesDao.listarHospedes();
+    }
+
+    public List<Hospedes> listarHospedesId(String id) {
+        return this.hospedesDao.buscarId(id);
+    }
+
+    public void atualizar(String nome, String sobrenome, Date dataNascimento, String nacionalidade, String telefone, Integer idReserva, Integer id) {
+        this.hospedesDao.atualizar(nome, sobrenome, dataNascimento, nacionalidade, telefone, idReserva, id);
+    }
+
+    public void deletar(Integer id) {
+        this.hospedesDao.deletar(id);
+    }
+}
---
+++ src/jdbc/controller/ReservaController.java
@@ -0,0 +1,38 @@
+package jdbc.controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
+
+import jdb.factory.ConnectionFactory;
+import jdbc.dao.ReservaDao;
+import jdbc.modelo.Reserva;
+
+public class ReservaController {
+    private ReservaDao reservaDao;
+
+    public ReservaController() {
+        Connection connection = new ConnectionFactory().recuperarConexao();
+        this.reservaDao = new ReservaDao(connection);
+    }
+
+    public void salvar(Reserva reserva) {
+		this.reservaDao.salvar(reserva);
+	}
+		
+	public List<Reserva> buscar() {
+		return this.reservaDao.buscar();
+	}
+	
+	public List<Reserva> buscarId(String id) {
+		return this.reservaDao.buscarId(id);
+	}
+	
+	public void atualizar(Date dataEntrada, Date dataSaida, String valor, String formaPagamento, Integer id) {
+		this.reservaDao.atualizar(dataEntrada, dataSaida, valor, formaPagamento, id);
+	}
+	
+	public void deletar(Integer id) {
+		this.reservaDao.deletar(id);
+	}
+}
